80fe52e927fbbd3281e8e64b1790a25373cbf27c
tarunnagb <tarunnagb@gmail.com>
2023-06-29 10:32:13 +0530

    add

---
+++ demoapp1/demoapp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter the number:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the 2nd number:");
+int b = Convert.ToInt32(Console.ReadLine());
+float sum = a + b;
+Console.WriteLine(sum);
+float sub = a - b;
+Console.WriteLine(sub);
